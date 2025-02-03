dd6a36723a9848566d181442afce185f77b27ba9
Hallowdread <hallowpahe@gmail.com>
2025-02-03 01:40:18 +0100

    Making the StarRating componenet more resuable

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "./App.jsx";
 import StarRating from "./StarRating.jsx";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="green" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} star</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <StarRating />
+    <Test />
   </StrictMode>
 );
